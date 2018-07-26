1e71c2378d65b75899601a4a5df9cf4dc40a3523
pareshkumar25 <40920931+pareshkumar25@users.noreply.github.com>
2018-07-27 00:24:05 +0530

    Initial commit

---
+++ aboutus.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>prakash tour</title>
+<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+<link href="stylecss.css" rel='stylesheet' type='text/css'/>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css'/>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--js--> 
+<script src="js/jquery.min.js"></script>
+
+<!--/js-->
+<!--animated-css-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+<script>
+ new WOW().init();
+</script>
+</head>
+
+<body>
+<?php include('function.php'); ?>
+<?php include('top.php'); ?>
+<!--/sticky-->
+<?php include('slider.php'); ?>
+<div style="height:50px"></div>
+
+
+
+
+<div style="width:900px; padding-left:100px">
+<table cellpadding="0px" cellspacing="0" width="900px">
+<tr><td class="headingText">About Us</td></tr>
+<tr><td class="paraText" width="900px">
+<b>About My Tour</b></br>
+
+
+
+</td></tr>
+<tr><td colspan="3"><img src="images/ttttt.jpg" width="1200px" height="700px"/></td></tr>
+
+<tr><td colspan="3" class="paraText"><div class="container">		  
+		 <div class="about-grids">
+			 <h4><b><span class="icon1"></span>Our Vision</b></h4>
+			 
+		 </div></td></tr>
+		<tr><td class="paraText"> <div class="about-grids grid2">
+			 <h4><b><span class="icon2"></span>Our Mission</b></h4>
+			
+
+ </div></td></tr>
+		<tr> <td class="paraText"><div class="about-grids">
+			 <h4><b><span class="icon3"></span>Safety Information</b></h4>
+			Vacation is a time to relax in safe surroundings.For emergency aid of any kind, call 911 from any phone... in your hotel, dial 9-911.
+ some safety tips are:<br/><br/>
+<b>TRANSPORTATION SAFETY TIPS:</b><br/>
+
+
+<b>HOTEL SAFETY TIPS: </b><br/><br/>
+
+
+		 </div>
+	 </div>
+    </td> </tr></table>
+
+</div>
+
+
+
+<div style="clear:both"></div>
+
+<?php include('bottom.php'); ?>
+</body>
+</html>
